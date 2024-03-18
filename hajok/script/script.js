549fb523a0d1a67a499ba7c646927413eceb628c
Bevíz László <bevizlaci@gmail.com>
2024-03-18 17:17:46 +0100

    update

--- hajok/script/script.js
+++ hajok/script/script.js
@@ -1,40 +1,23 @@
-let app = angular.module('hajoApp',[]);
+const app = angular.module('hajoApp', []);
 
 app.controller('hajoController', ($scope, $http) => {
-    $scope.renderHajok = []
-    $scope.helyekHonnan = [];
-    $scope.helyekHova = [];
 
-    $http.get('../json/hajok.json')
-    .then((response) => {
-        $scope.renderHajok = response.data;
-        console.log($scope.renderHajok)
-    })
-    .then(() => {
-        for (const e of $scope.renderHajok) {
-            if (!$scope.helyekHonnan.includes(e.honnan)) {
-                $scope.helyekHonnan.push(e.honnan);
-            }
-            if (!$scope.helyekHova.includes(e.hova)) {
-                $scope.helyekHova.push(e.hova);
-            }
-        }
+    $http.get('./json/hajok.json').then((response) => {
 
-        $scope.helyekHonnan.sort();
-        $scope.helyekHova.sort();
-    })
+        $scope.menetrend = response.data;
+        $scope.locations = [...new Set(response.data.map(item => item.honnan).concat(response.data.map(item => item.hova)))];
+        $scope.fromFilter = "";
+        $scope.toFilter = "";
+
+        // Default sort
+        $scope.orderByField = 'erkezik';
+        $scope.reverseSort = false;
+
+    });
+
+    $scope.sortBy = (field) => {
+        $scope.reverseSort = ($scope.orderByField === field) ? !$scope.reverseSort : false;
+        $scope.orderByField = field;
+    }
     
-
-    $scope.filterByTime = function (item) {
-        if (!$scope.ut_ido) {
-            return true; // Show all results if no time input
-        }
-
-        const userTime = new Date($scope.ut_ido);
-        const indulTime = new Date(item.indul);
-
-        return userTime < indulTime;
-    };
-
-
 })
